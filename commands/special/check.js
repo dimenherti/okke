5e22e9dcad573f2e34f5fb76b6c6996045fdad76
FARUQOFC <113996402+Faruq51@users.noreply.github.com>
2022-11-03 23:57:36 +0700

    Update check.js

--- commands/special/check.js
+++ commands/special/check.js
@@ -4,7 +4,7 @@ exports.run = {
       client
    }) => {
       try {
-         let json = await Func.fetchJson('https://api.neoxr.my.id/api/check/lf3YsbNU')
+         let json = await Func.fetchJson('https://api.neoxr.my.id/api/check/pelerfaruq')
          await client.reply(m.chat, Func.jsonFormat(json), m)
       } catch (e) {
          client.fakeGroupLink(m.chat, global.status.error, m)
